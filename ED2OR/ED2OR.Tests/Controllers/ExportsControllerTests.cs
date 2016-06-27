b7185cfe935b6fb2092ddd5ec69bc02ba4eb4ef2
Eduardo Fonseca <eduardo.fonseca@pticostarica.com>
2016-06-27 13:10:53 -0600

    adding tests for ExportsController Index action method

--- ED2OR/ED2OR.Tests/Controllers/ExportsControllerTests.cs
+++ ED2OR/ED2OR.Tests/Controllers/ExportsControllerTests.cs
@@ -10,6 +10,7 @@ using System.Web;
 using ED2OR.Tests.Utils;
 using Moq;
 using System.Web.Routing;
+using ED2OR.ViewModels;
 
 namespace ED2OR.Controllers.Tests
 {
@@ -74,6 +75,58 @@ namespace ED2OR.Controllers.Tests
                 Assert.IsNotNull(viewModel.CoursesCriteriaSection, "Invalid CoursesCriteriaSection");
                 Assert.IsNotNull(viewModel.TeachersCriteriaSection, "Invalid TeachersCriteriaSection");
                 Assert.IsNotNull(viewModel.SectionsCriteriaSection, "Invalid SectionsCriteriaSection");
+                var defaultExportViewModel = viewResult.Model as ED2OR.ViewModels.ExportsViewModel;
+                var resultPreviewCmd = await controller.Index(defaultExportViewModel, "Preview");
+                Assert.IsNotNull(resultPreviewCmd, "Invalid Preview Post reuslt");
+                Assert.IsInstanceOfType(resultPreviewCmd, typeof(ViewResult), "Unexpected result type");
+                ViewResult previewCmdViewResult = resultPreviewCmd as ViewResult;
+                Assert.IsInstanceOfType(previewCmdViewResult.Model, typeof(ExportsViewModel), "Invalid model");
+                var previewCmdModel = previewCmdViewResult.Model as ExportsViewModel;
+                Assert.IsNotNull(previewCmdModel.DataPreviewSections, "Invalid Data Preview Sections");
+
+
+                var jsonPreviews = await Utils.ApiCalls.GetJsonPreviews();
+                Assert.AreEqual(previewCmdModel.JsonPreviews.AcademicSessions, jsonPreviews.AcademicSessions, "JSON Preview AcademicSessions do not match");
+                Assert.AreEqual(previewCmdModel.JsonPreviews.Classes, jsonPreviews.Classes, "JSON Preview Classes do not match");
+                Assert.AreEqual(previewCmdModel.JsonPreviews.Courses, jsonPreviews.Courses, "JSON Preview Courses do not match");
+                Assert.AreEqual(previewCmdModel.JsonPreviews.Enrollments, jsonPreviews.Enrollments, "JSON Preview  Enrollments do not match");
+                Assert.AreEqual(previewCmdModel.JsonPreviews.Orgs, jsonPreviews.Orgs, "JSON Preview Orgs do not match");
+                Assert.AreEqual(previewCmdModel.JsonPreviews.Users, jsonPreviews.Users, "JSON Preview Users do not match");
+
+                var orgColumnNames = typeof(CsvOrgs).GetProperties().Select(x => x.Name);
+                var usersColumnNames = typeof(CsvUsers).GetProperties().Select(x => x.Name);
+                var coursesColumnNames = typeof(CsvCourses).GetProperties().Select(x => x.Name);
+                var classesColumnNames = typeof(CsvClasses).GetProperties().Select(x => x.Name);
+                var enrollmentsColumnNames = typeof(CsvEnrollments).GetProperties().Select(x => x.Name);
+                var academicsessionsColumnNames = typeof(CsvAcademicSessions).GetProperties().Select(x => x.Name);
+
+                CompareColumns(orgColumnNames, previewCmdModel.DataPreviewSections, "orgs");
+                CompareColumns(usersColumnNames, previewCmdModel.DataPreviewSections, "users");
+                CompareColumns(coursesColumnNames, previewCmdModel.DataPreviewSections, "courses");
+                CompareColumns(classesColumnNames, previewCmdModel.DataPreviewSections, "classes");
+                CompareColumns(enrollmentsColumnNames, previewCmdModel.DataPreviewSections, "enrollments");
+                CompareColumns(academicsessionsColumnNames, previewCmdModel.DataPreviewSections, "academicsessions");
+
+                System.Web.Fakes.ShimHttpServerUtility.AllInstances.MapPathString = (server, path) =>
+                {
+                    return System.IO.Path.Combine(Environment.CurrentDirectory, "FakeMappedPAth", path);
+                };
+                var downloadResult = await controller.Index(defaultExportViewModel, "Download");
+                Assert.IsNotNull(downloadResult, "Invalid Download reuslt");
+                Assert.IsInstanceOfType(downloadResult, typeof(FileContentResult), "Invalid File Result");
+                FileContentResult fileResult = downloadResult as FileContentResult;
+                Assert.IsTrue(fileResult.FileContents != null && fileResult.FileContents.Length > 0, "Invalid File Contents");
+            }
+        }
+
+        private void CompareColumns(IEnumerable<string> sourceTypeColumnNames, List<DataPreviewSection> dataPreviewSections, string sectionName)
+        {
+
+            var dataPreviewColumnNames = dataPreviewSections.Where(p => p.SectionName == sectionName).FirstOrDefault().ColumnNames;
+            foreach (var singleSourceColumn in sourceTypeColumnNames)
+            {
+                Assert.IsTrue(dataPreviewColumnNames.Contains(singleSourceColumn),
+                    string.Format("Column {0} does not exist in Data Preview for {1}", singleSourceColumn, sectionName));
             }
         }
     }
