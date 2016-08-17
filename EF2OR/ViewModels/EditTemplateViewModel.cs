c9a9f0382fdee1dce936c91038c5f91206d247c2
Kevin <kevin.o.seymour@gmail.com>
2016-08-17 10:08:16 -0400

    templates allow special characters

---
+++ EF2OR/ViewModels/EditTemplateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EF2OR.ViewModels
+{
+    public class EditTemplateViewModel
+    {
+        public int editTemplateId { get; set; }
+        public string editTemplateName { get; set; }
+        public string editTemplateVendorName { get; set; }
+    }
+}
